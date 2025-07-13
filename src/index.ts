8b3cba0b375cede9629d4e31e0ed61fdb3a700ae
blasco <blascoburguillos@gmail.com>
2025-07-13 18:34:50 +0200

    fix: update package.json paths and clean up index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 // Main exports for the @j2blasco/ts-boundaries package
-export {
-  generateBoundariesConfig as generateBoundaries,
-  generateBoundariesConfigFile,
-  type GeneratorOptions,
-} from './core/generator.js';
 export { type Boundaries } from './core/boundaries.types.js';
